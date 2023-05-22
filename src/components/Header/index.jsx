4ec98717096872289af4049195e6c6ae3226597d
Antonio Alan <tonio.alan07@gmail.com>
2023-05-22 15:07:50 -0300

    add estilo do Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,15 @@
+import styles from "./styles.module.scss";
 import Logo from '../../assets/logo.png'
 import Search from '../../assets/search.png'
 
 export function Header() {
     return (
-        <header>
+        <header className={styles.header}>
             <img src={Logo} alt="Logo do Alura Space" />
-            <div>
-                <input type="text" placeholder="O que você procura?" />
+            <div className={styles.header__container}>
+                <input 
+                    className={styles.header__input} 
+                    type="text" placeholder="O que você procura?" />
                 <img src={Search} alt="Ícone de lupa" />
             </div>
         </header>
