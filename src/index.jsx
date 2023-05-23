570d40e70809063cd9ee55c694fee14d8b3e9fad
Antonio Alan <tonio.alan07@gmail.com>
2023-05-23 01:13:09 -0300

    add fonts

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './styles/generic/global.scss';
+import './styles/settings/colors.css';
+import './assets/fonts/GandhiSans-Regular.otf';
+import './assets/fonts/GandhiSans-Bold.otf';
 import App from './App.jsx'
-import './styles/generic/global.scss'
-import './styles/settings/colors.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
