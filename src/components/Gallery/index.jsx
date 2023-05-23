89dfdc849c17ab08680a249985f7b9a03c148449
Antonio Alan <tonio.alan07@gmail.com>
2023-05-23 09:22:09 -0300

    criação do Tags components

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,10 +1,12 @@
 import styles from "./styles.module.scss";
+import { Tags } from "../Tags";
 import React from 'react'
 
 export function Gallery() {
   return (
     <section>
         <h2>Navegue pela galeria</h2>
+        <Tags />
         <ul></ul>
     </section>
   )
---
+++ src/components/Tags/index.jsx
@@ -0,0 +1,14 @@
+
+export function Tags() {
+  return (
+    <div>
+        <p>Filtro por tags:</p>
+        <ul>
+            <li>Estrelas</li>
+            <li>Galáxias</li>
+            <li>Lua</li>
+            <li>Planetas</li>
+        </ul>
+    </div>
+  )
+}
